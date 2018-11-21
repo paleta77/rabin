ed2878436d1080d0623d8adcfd3a1118767ed166
Paweł Szrubas <pawel050673@gmail.com>
2018-11-21 14:06:45 +0100

    nie wiem skąd wziać to -3 i 2

--- rabin/src/rabin/Rabin.java
+++ rabin/src/rabin/Rabin.java
@@ -8,7 +8,9 @@ package rabin;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import static java.lang.Math.abs;
 import java.nio.file.Files;
+import static rabin.Rabin.compute;
 
 /**
  *
@@ -44,26 +46,30 @@ public class Rabin {
         fop.close();
     }
  
-public static int gcdExtended(int a, int b, int x, int y) 
-    { 
-        // Base Case 
-        if (a == 0) 
-        { 
-            x = 0; 
-            y = 1; 
-            return b; 
-        } 
-  
-        int x1=1, y1=1; // To store results of recursive call 
-        int gcd = gcdExtended(b%a, a, x1, y1); 
-  
-        // Update x and y using results of recursive 
-        // call 
-        x = y1 - (b/a) * x1; 
-        y = x1; 
-        //System.out.println(x + " " + y);
-        return gcd; 
-    } 
+    public static void compute(long a, long b)
+	    {
+                long res_x,res_y;
+	        long x = 0, y = 1;
+	        long prevx = 1, prevy = 0, temp;
+	        while (b != 0)
+	        {
+	            long q = a / b;
+	            long r = a % b;
+	            a = b;
+	            b = r;
+	 
+	            temp = x;
+	            x = prevx - q * x;
+	            prevx = temp;
+	 
+	            temp = y;
+	            y = prevy - q * y;
+	            prevy = temp;            
+	        }
+	        System.out.println("Roots  x : "+ prevx +" y :"+ prevy);
+	        res_x = prevx;
+	        res_y = prevy;
+}
     
 
 
@@ -71,26 +77,41 @@ public static int gcdExtended(int a, int b, int x, int y)
         int p = 7;
         int q = 11;
         int klucz = generujKlucz(p, q);
+        System.out.println("klucz " + klucz);
+        int M = 68;
+        int c = (M*M)%klucz;
         
-        int c = (10*10)%klucz;
-        
-        System.out.println(c);
+        System.out.println("c " + c);
         int mp = (int)((Math.pow(c, 0.25*(p+1)))%p);
         int mq = (int)((Math.pow(c, 0.25*(q+1)))%q);
-        int x=1, y=0;
-        int g = gcdExtended(mp, mq, x, y); 
-        System.out.println("gcd(" + mp +  " , " + mq+ ") = " + g); 
-        System.out.println(x + " " + y);
-        
-        int r = (int)(-3*p*mq+2*q*mp)%klucz;
-        int _r = (int)(klucz - r);
-        int s = (int)(-3*p*mq-2*q*mp)%klucz;
-        int _s = (int)(klucz - s);     
-        
+
         System.out.println(mp + " "+ mq);
+        
+        int m1 = (int)(Math.pow(c,(p+1)/4)%p);
+        int m3 = (int)(Math.pow(c,(q+1)/4)%q);
+        int m2 = (int)(p - Math.pow(c,(p+1)/4)%p);
+        int m4 = (int)(q - Math.pow(c,(q+1)/4)%q);
+        
+        int a = (int)(q*(Math.pow(q, -1)%p));
+        int b = (int)(p*(Math.pow(p, -1)%q));
+        
+        System.out.println("a: " + a + " b:" + b);
+        
+        int M1 = (int)(a*m1+b*m3)%klucz;
+        int M2 = (int)(a*m1+b*m4)%klucz;
+        int M3 = (int)(a*m2+b*m3)%klucz;
+        int M4 = (int)(a*m2+b*m4)%klucz;
+        
+        int r = abs((int)((-3*p*mq)+(2*q*mp))%klucz);
+        int _r = abs((int)(klucz - r));
+        int s = abs((int)((-3*p*mq)-(2*q*mp))%klucz);
+        int _s = abs((int)(klucz - s));     
+        
+        
         System.out.println(r + " "+ _r + " " + s + " " + _s);
-        //System.out.println(M1 + " "+ M2 + " " + M3 + " " + M4);
-        //System.out.println(M1 + " "+ M2 + " " + M3 + " " + M4);
+        
+       //System.out.println(m1 + " "+ m2 + " " + m3 + " " + m4);
+       //System.out.println(M1 + " "+ M2 + " " + M3 + " " + M4);
         
         
     }
